ea5b1685004291815b9957a6ec22bbaa0b32d89b
dunCan <f0uad.f@hotmail.com>
2023-02-20 08:50:07 +0300

    updated project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -35,7 +35,15 @@ const Projects = () => {
             my knowledge in technology.
           </ProjectGridItem>
         </Section>
-        <Section>
+      </SimpleGrid>
+
+      <Section delay={0.2}>
+        <Divider my={6} />
+        <Heading as="h3" fontSize={20} mb={4}>Collaborations</Heading>
+      </Section>
+
+      <SimpleGrid columns={[1,1,2]} gap={6}>
+        <Section delay={0.3}>
           <ProjectGridItem
             id="geoservices"
             title="S.B. Géoservices Inc."
